e9ee5f97fe8cd3445a8f789d2d8998bf6a161d13
JeongDongUk <foodsmith96@naver.com>
2024-05-31 17:30:49 +0900

    Initial commit

---
+++ src/main/java/io/spring/websocket/controller/ChatController.java
@@ -0,0 +1,58 @@
+package io.spring.websocket.controller;
+
+import jakarta.websocket.OnClose;
+import jakarta.websocket.OnMessage;
+import jakarta.websocket.OnOpen;
+import jakarta.websocket.Session;
+import jakarta.websocket.server.PathParam;
+import jakarta.websocket.server.ServerEndpoint;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.*;
+
+@Slf4j
+@Controller
+@ServerEndpoint("/websocket")
+public class ChatController extends Socket {
+
+    private static final List<Session> sessions = new ArrayList<>();
+
+
+    @GetMapping("/chatroom")
+    public String index() {
+        System.out.println("dddd");
+        return "index";
+    }
+
+
+    @OnOpen
+    public void open(Session newUser) {
+        System.out.println("connected");
+        sessions.add(newUser);
+        System.out.println(newUser.getId());
+    }
+
+
+    @OnMessage
+    public void getMsg(Session recieveSession, String msg) {
+        for (int i = 0; i < sessions.size(); i++) {
+            if (!recieveSession.getId().equals(sessions.get(i).getId())) {
+                try {
+                    sessions.get(i).getBasicRemote().sendText("상대 : "+msg);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }else{
+                try {
+                    sessions.get(i).getBasicRemote().sendText("나 : "+msg);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
